00aaa65eddafb74d49aef14ee1777ba8f38b1d4f
MaagdalenaMatoga <magdalena.matoga@polsatmedia.pl>
2023-05-24 21:00:22 +0200

    poprawione Intro

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-projekt
+Projekt instrukcja uruchomienia. 
 
 1. Przejść do katalogu backend w konsoli 
 2. Wpisać json-server --watch db.json
-3. Ww innej konsoli przejśc (np w VSC terminal) do katalogu frontend
+3. W innej konsoli przejśc (np w VSC terminal) do katalogu frontend
 4. Wpisać npm start 
 5. Potwierdzić zmianę portu (Y) 
 
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,16 +4,15 @@ import styles from './Intro.module.scss'
 function Intro() {
     return (
        
-     
       <div className={styles.introsection}> 
       <div className={styles.text}>
-               <h3 className={styles.header}>Fresh delivery</h3>
-            <h1 className={styles.subheader}>Ekologiczne<br/>warzywa test
+               <h3 className={styles.header}>Zdrowe posiłki</h3>
+            <h1 className={styles.subheader}>Świeże<br/>składniki
             </h1>
             </div>
-
-
-
+          
+    
+      
       </div>
 
     );
