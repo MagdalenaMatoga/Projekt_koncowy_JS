d44b12465e8c1154abfafc235aa76f530ab12c03
MaagdalenaMatoga <magdalena.matoga@polsatmedia.pl>
2023-05-29 22:26:41 +0200

    jak działa poprawione + kafelki

--- src/components/delivery/Delivery.js
+++ src/components/delivery/Delivery.js
@@ -1,8 +1,8 @@
 import styles from './Delivery.module.scss'
-import car from './images/car.png'
 import logistics from './images/logistics.png'
 import house from './images/house.png'
 import fast from './images/fast-delivery.png'
+import vegetables from './images/vegetables.png'
 
 
 function Delivery() {
@@ -11,27 +11,27 @@ function Delivery() {
       <h2 className={styles.deliveryTitle}>Jak to działa?</h2>
       <div className={styles.deliverySection}> 
       <div className={styles.box} >
-        <img className={styles.image} src={car} alt=""></img>
-               <h5 className={styles.header}>Lorem Ipsum</h5>  
-               <p className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy tthe 1500s</p>   
+        <img className={styles.image} src={house} alt=""></img>
+               <h5 className={styles.header}>Wygodny wybór dań</h5>  
+               <p className={styles.description}>Oferujemy szeroki wybór dań, które możesz łatwo i wygodnie w zaciszu domowym wybrać spośród naszej bogatej oferty. Zapewniamy różnorodność opcji, abyś mógł zawsze znaleźć coś dla siebie.</p>   
+
+            </div>
+            <div className={styles.box} >
+        <img className={styles.image} src={vegetables} alt=""></img>
+               <h5 className={styles.header}>Przygotowanie świeżych składników</h5>  
+               <p className={styles.description}>Nasze dania są przygotowywane z najświeższych składników, które starannie dobieramy. Stawiamy na jakość i dbamy o to, aby każdy składnik był świeży i wysokiej jakości, aby dostarczyć Ci wyjątkowe doznania kulinarne.</p>   
 
             </div>
             <div className={styles.box} >
         <img className={styles.image} src={logistics} alt=""></img>
-               <h5 className={styles.header}>Lorem Ipsum</h5>  
-               <p className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>   
-
-            </div>
-            <div className={styles.box} >
-        <img className={styles.image} src={house} alt=""></img>
-               <h5 className={styles.header}>Lorem Ipsum</h5>  
-               <p className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>   
+               <h5 className={styles.header}>Przygotowanie wysyłki</h5>  
+               <p className={styles.description}> Po złożeniu zamówienia, nasz zespół profesjonalistów rozpoczyna proces przygotowania wysyłki. Dbamy o staranne zapakowanie zamówionych składników, aby dotarły do Ciebie w nienaruszonym stanie.</p>   
 
             </div>
             <div className={styles.box} >
         <img className={styles.image} src={fast} alt=""></img>
-               <h5 className={styles.header}>Lorem Ipsum</h5>  
-               <p className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>   
+               <h5 className={styles.header}>Szybka dostawa</h5>  
+               <p className={styles.description}>Gwarantujemy szybką dostawę zamówionych produktów pod wskazany adres. Nasz zespół dostawców działa sprawnie i terminowo, abyś mógł cieszyć się naszymi smakołykami w najkrótszym możliwym czasie.</p>   
 
             </div>
     
--- src/pages/dishes/Dishes.js
+++ src/pages/dishes/Dishes.js
@@ -1,16 +1,42 @@
+import DishCard from '../../components/dishcard/DishCard';
+import styles from '../../components/dishcard/DishCard.module.scss'
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
-import DishCard from "../../components/dishcard/DishCard";
+
+
 
 function Dishes() {
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    axios.get('http://localhost:3000/dishes')
+    .then(function (response) {
+      // handle success
+      console.log(response.data); 
+      setData(response.data);
+    })
+  },[]) //[]odpal się tylko jak pojawi się pierwszy raz
+
+
+  //jeżeli istenieje jedno i drugie to zwróc to drugie
+
     return (
-      <div>
-        <h2>Dishes</h2>
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
+    
+      data && <div> 
+        <h2 className={styles.dishTitle}>Dishes</h2>
+  
+        <div className={styles.dishList}>
+        <DishCard  cardData={data[0]} />
+        <DishCard cardData={data[1]} />
+        <DishCard cardData={data[2]} />
+        <DishCard cardData={data[3]} />
+        <DishCard cardData={data[4]} />
+        <DishCard cardData={data[5]} />
+        <DishCard cardData={data[6]} />
+        <DishCard cardData={data[7]} />
+  
+        
+        </div>
       </div>
     );
   }
